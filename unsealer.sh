0807e6dbc40382979c239ce3fbb48ec9e5a6b949
Giel Goudsmit <giel@goudsmit.tech>
2020-05-11 15:42:36 +0200

    Fixed unsealer for new vault API using /sys/health

--- unsealer.sh
+++ unsealer.sh
@@ -13,24 +13,40 @@ cat <<EOF
 EOF
 }
 
+
 echo "trying to unseal vault..."
 FIRST=1
 
+STATUS_URL="http://127.0.0.1:8080/v1/sys/health"
+
 while true
 do
-  STATUS=$(curl -s -o /dev/null -w '%{http_code}' http://127.0.0.1:8080)
+
+  STATUS=$(curl -s -o /dev/null -w '%{http_code}' $STATUS_URL)
   echo "status = ${STATUS}"
-  if [ ${STATUS} -eq 404 ]; then
-    echo "listener is up"
-	curl -X PUT -d "$(generate_post_data)" http://127.0.0.1:${PORT:?}/v1/sys/unseal
-	echo "done unsealing"
-    break
-  else
+
+  case $STATUS in
+  200) echo "initialized, unsealed and active" ; break ;;
+  429) echo "unsealed and standby" ; break ;;
+  427) echo "active in disaster recovery mode" ; break ;;
+  473) echo "performance standby" ; break ;;
+  501) echo "not initialized" ; break ;;
+
+  503)
+    echo "listening and sealed; sending unseal key"
+	curl -s -X PUT -d "$(generate_post_data)" http://127.0.0.1:${PORT:?}/v1/sys/unseal
+  ;;
+
+  *)
     if [ ${FIRST} -eq 1 ]; then
-        echo "waiting for listener..."
+        echo "waiting for listener.."
         FIRST=0
     fi
+
     echo -n "."
-  fi
-  sleep 1
+    sleep 1
+  ;;
+
+  esac
+
 done
